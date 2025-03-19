0d640c2cac9bcb07a720c19be07c368068a46440
8208221530 <1173760090@qq.com>
2025-03-19 21:35:36 +0800

    设定路由，引入element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
-<script setup></script>
-
+<script setup>
+import { useRouter, useRoute } from 'vue-router'
+const router = useRouter()
+const route = useRoute()
+console.log(router, route)
+</script>
 <template>
   <div>我是App</div>
+  <button @click="$router.push('/hh')">tiaotiao</button>
+  <div>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+    ...
+  </div>
 </template>
 
 <style scoped></style>
