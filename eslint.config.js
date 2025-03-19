8338e285bff431fee80a7f4ed26ec41b6ba6d60f
8208221530 <1173760090@qq.com>
2025-03-19 20:21:31 +0800

    begin

---
+++ eslint.config.js
@@ -0,0 +1,61 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import eslintConfigPrettier from '@vue/eslint-config-prettier' //添加这一行代码
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}'],
+    //rules添加到这里，上面写的该规则应用于js,mjs,jsx,vue
+    rules: {
+      'prettier/prettier': [
+        'warn',
+        {
+          singleQuote: true,
+          semi: false,
+          printWidth: 100,
+          trailingComma: 'none',
+          endOfLine: 'auto'
+        }
+      ],
+      'vue/multi-word-component-names': ['warn', { ignores: ['index'] }],
+      'vue/no-setup-props-destructure': ['off'],
+      'no-undef': 'error'
+    }
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    //rules添加到这里，这里应该也是莫一种文件吧加不加无所谓
+    rules: {
+      'prettier/prettier': [
+        'warn',
+        {
+          singleQuote: true,
+          semi: false,
+          printWidth: 100,
+          trailingComma: 'none',
+          endOfLine: 'auto'
+        }
+      ],
+      'vue/multi-word-component-names': ['warn', { ignores: ['index'] }],
+      'vue/no-setup-props-destructure': ['off'],
+      'no-undef': 'error'
+    }
+  },
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      }
+    }
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  skipFormatting,
+  eslintConfigPrettier //添加这一行代码
+]
